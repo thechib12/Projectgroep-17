9bf17ba2855aa8eac73d0536d0bd95222f561d8d
boschma2702 <reneboschma@hotmail.com>
2015-11-03 11:16:08 +0100

    fix #?

--- Cursor.py
+++ Cursor.py
@@ -187,9 +187,9 @@ class xyGetter(threading.Thread):
                 self.last_y = 45
             """
             len = math.tan(math.radians(self.last_y))*1080
-            y = len - math.cos(math.radians(self.last_x))*len
-            # y = len - math.abs(math.cos(math.radians(self.last_x))*len)
-            x = 540 + len * math.sin(math.radians(self.last_x))
+            # y = len - math.cos(math.radians(self.last_x))*len
+            y = len - math.abs(math.cos(math.radians(self.last_x))*len)
+            x = 960 + len * math.sin(math.radians(self.last_x))
             self.cursor.set_pos_toset([x, y])
 
             print("{0:.2f} {1:.2f} {2:.2f} {2:.2f}".format(self.last_x, self.last_y, x, y))
